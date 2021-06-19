05138c78ada5dd2e225e828eaaceb3a020215411
Moustafa Mohamed Sokkar <mostafasookar@hotmail.com>
2021-06-20 00:31:58 +0300

    Update CookieService.cs

--- NationalCookies.Data/Services/CookieService.cs
+++ NationalCookies.Data/Services/CookieService.cs
@@ -1,24 +1,38 @@
 ﻿using NationalCookies.Data.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace NationalCookies.Data.Services
 {
     public class CookieService : ICookieService
     {
         private CookieContext _context;
+        private IDistributedCache _cache;
 
-        public CookieService(CookieContext context)
+        public CookieService(CookieContext context, IDistributedCache cache )
         {
             _context = context;
+            _cache = cache;
         }
 
         public List<Cookie> GetAllCookies()
         {
             List<Cookie> cookies;
 
+
+            var cachedCookies = _cache.GetString("cookies");
+            if (!string.IsNullOrEmpty(cachedCookies)){
+            
+                cookies = JsonConvert.DeserializeObject<List<Cookie>>(cachedCookies);
+            
+            }
+            else
+            {
             //get the cookies from the database
             cookies = _context.Cookies.ToList();
+            
+            _cache.SetString("cookies", JsonConvert.SerializeObject(cookies), options);
 
             return cookies;
         }
