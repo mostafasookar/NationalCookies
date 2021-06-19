8c894b857a1448276d46a34ace7843fad0d62075
Moustafa Mohamed Sokkar <mostafasookar@hotmail.com>
2021-06-20 00:44:37 +0300

    Update CookieService.cs

--- NationalCookies.Data/Services/CookieService.cs
+++ NationalCookies.Data/Services/CookieService.cs
@@ -1,4 +1,5 @@
-﻿using NationalCookies.Data.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using NationalCookies.Data.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
@@ -10,7 +11,7 @@ namespace NationalCookies.Data.Services
         private CookieContext _context;
         private IDistributedCache _cache;
 
-        public CookieService(CookieContext context, IDistributedCache cache )
+        public CookieService(CookieContext context, IDistributedCache cache = null)
         {
             _context = context;
             _cache = cache;
@@ -19,21 +20,39 @@ namespace NationalCookies.Data.Services
         public List<Cookie> GetAllCookies()
         {
             List<Cookie> cookies;
+            
+            //if no cache is passed into the constructor
+            //of this service, just get the cookies from the database and return them
+            if(_cache == null)
+            {
+                return cookies = _context.Cookies.ToList();                
+            }
 
-
+            //first, try to get cookies from cache
             var cachedCookies = _cache.GetString("cookies");
             if (!string.IsNullOrEmpty(cachedCookies)){
-            
+
+                //if they are there, deserialize them
                 cookies = JsonConvert.DeserializeObject<List<Cookie>>(cachedCookies);
             }
             else
             {
-            //get the cookies from the database
-            cookies = _context.Cookies.ToList();
-            
-            _cache.SetString("cookies", JsonConvert.SerializeObject(cookies), options);
+                //if no cookies in are in cache yet, get them from the database
+                cookies = _context.Cookies.ToList();
+
+                DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
+                options.SetAbsoluteExpiration(new System.TimeSpan(0, 0, 15));
+
+                //and then, put them in cache
+                _cache.SetString("cookies", JsonConvert.SerializeObject(cookies), options);
+            }
 
             return cookies;
+        }
+
+        public void ClearCache()
+        {
+            _cache.Remove("cookies");
         }
     }
 }
